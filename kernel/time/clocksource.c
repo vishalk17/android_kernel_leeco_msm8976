5da82dee0f2eadce8e3cda06b928af40678eb041
Thomas Gleixner <tglx@linutronix.de>
2013-05-28 09:48:46 +0200

    clocksource: Implement clocksource_select_fallback() for CONFIG_ARCH_USES_GETTIMEOFFSET=y
    commit 7eaeb34305 (clocksource: Provide unbind interface in sysfs)
    implemented clocksource_select_fallback() which is not defined for
    CONFIG_ARCH_USES_GETTIMEOFFSET=y. Add an empty inline function for
    that.
    
    Reported-by: Ingo Molnar <mingo@kernel.org>
    Reported-by: fengguang.wu@intel.com
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

--- kernel/time/clocksource.c
+++ kernel/time/clocksource.c
@@ -646,6 +646,7 @@ static void clocksource_select_fallback(void)
 #else /* !CONFIG_ARCH_USES_GETTIMEOFFSET */
 
 static inline void clocksource_select(void) { }
+static inline void clocksource_select_fallback(void) { }
 
 #endif
 
