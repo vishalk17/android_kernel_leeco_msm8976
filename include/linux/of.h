a55461bc77f3c9bfa37925974fe43ec9fbdeb97e
Rob Herring <rob.herring@calxeda.com>
2013-09-17 10:42:50 -0500

    of: implement of_node_to_nid as a weak function
    Implement of_node_to_nid as weak function to remove the dependency on
    asm/prom.h. This is in preparation to make prom.h optional.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Acked-by: Grant Likely <grant.likely@linaro.org>

--- include/linux/of.h
+++ include/linux/of.h
@@ -540,13 +540,10 @@ static inline int of_device_is_stdout_path(struct device_node *dn)
 #define of_match_node(_matches, _node)	NULL
 #endif /* CONFIG_OF */
 
-#ifndef of_node_to_nid
-static inline int of_node_to_nid(struct device_node *np)
-{
-	return numa_node_id();
-}
-
-#define of_node_to_nid of_node_to_nid
+#if defined(CONFIG_OF) && defined(CONFIG_NUMA)
+extern int of_node_to_nid(struct device_node *np);
+#else
+static inline int of_node_to_nid(struct device_node *device) { return 0; }
 #endif
 
 /**
